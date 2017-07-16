044ff58c560e22f12bbae75b3986e7aab700c1a3
Tamara Ortiz <tamara.tfs@gmail.com>
2017-07-16 07:50:21 -0400

    cambios en UI

--- hackathon2017/proyectandopy/src/pages/tutorial/tutorial.ts
+++ hackathon2017/proyectandopy/src/pages/tutorial/tutorial.ts
@@ -8,7 +8,7 @@ export class TutorialPage {
   slides = [
     {
       title: "Plan Nacional de Desarrollo 2030",
-      description: "El <b>Plan Nacional de Desallo 2030 o PND</b> busca coordinar las acciones de los Poderes de Estado, niveles de gobierno, sociedad civil y sector privado a fin de mejorar la gestión del País",
+      description: "El <b>Plan Nacional de Desarrollo 2030 o PND</b> busca coordinar las acciones de los Poderes del Estado, niveles de Gobierno, sociedad civil y sector privado a fin de mejorar la gestión del País",
       image: "assets/img/57.svg",
     },
     {
@@ -18,22 +18,22 @@ export class TutorialPage {
     },
     {
       title: "Niveles",
-      description: "Actualmente existen 11 <b>Niveles</b> que agrupan las distinatas entidades del Gobiendo, entre ellas estan los tres poderes del Estado, entidades y empresas publicas, universidades, entre otros.",
+      description: "Actualmente existen <b>11 Niveles</b> que agrupan las distintas entidades del Gobierno, entre ellas están los tres poderes del Estado, entidades y empresas públicas, universidades, entre otros.",
       image: "assets/img/24.svg",
     },
     {
       title: "Entidades",
-      description: "Las distintas <b>Entidade</b> del Gobiernos planean y ejecutan programas vinculados a los ejes estratégicos, con el fin de alcanzar la meta fijada en el Plan de Desarrollo 2030.",
+      description: "Las distintas <b>Entidades</b> del Gobierno planean y ejecutan programas vinculados a los ejes estratégicos, con el fin de alcanzar la meta fijada en el Plan de Desarrollo 2030.",
       image: "assets/img/08.svg",
     },
     {
       title: "Programas",
-      description: "Los <b>Programas</b> son un conjunto de acciones planeadas por cada entidad, que trabajan sobre un eje estratégico especifico y que cuenta con un presupuesto determinado",
+      description: "Los <b>Programas</b> son un conjunto de acciones planeadas por cada entidad, que trabajan sobre un eje estratégico específico y que cuenta con un presupuesto determinado.",
       image: "assets/img/36.svg",
     },
     {
       title: "ProyectandoPy",
-      description: "<b>ProyectandoPy</b> tomá los datos de los distintos programas, como número de beneficiarios y costos, y genera reportes y estadisticas útiles para ponerlos a disposición de los ciudadanos",
+      description: "<b>ProyectandoPy</b> toma los datos de los distintos programas, como número de beneficiarios y costos, y genera reportes y estadísticas útiles para ponerlos a disposición de los ciudadanos",
       image: "assets/img/44.svg",
     }
   ];
