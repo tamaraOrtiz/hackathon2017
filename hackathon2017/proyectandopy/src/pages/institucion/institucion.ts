cb4852133aeb93f9207576f7a5fbac4679441685
Tamara Ortiz <tamara.tfs@gmail.com>
2017-07-21 21:41:26 -0400

    mostrar cantidades en listar de entidades

--- hackathon2017/proyectandopy/src/pages/institucion/institucion.ts
+++ hackathon2017/proyectandopy/src/pages/institucion/institucion.ts
@@ -13,10 +13,26 @@ import { BasePage } from '../../app/base-page';
 
 export class InstitucionPage extends BasePage {
   items: Array<any>;
+  searchQuery: string = '';
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public dataService: InstitucionData) {
     super(navCtrl, navParams, dataService);
     this.where = "borrado = 'false'";
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.indexOf(val) > -1);
+      })
+    }
   }
 
   itemTapped(event, item) {
