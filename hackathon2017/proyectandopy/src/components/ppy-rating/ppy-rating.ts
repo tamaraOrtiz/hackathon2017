cf372a1d24aaea7543680d1fcd162d99ce444d3f
Ruben Bordon <bordonwork@gmail.com>
2017-08-08 21:17:06 -0400

    agregue un listener para actualizar el rating

--- hackathon2017/proyectandopy/src/components/ppy-rating/ppy-rating.ts
+++ hackathon2017/proyectandopy/src/components/ppy-rating/ppy-rating.ts
@@ -1,13 +1,28 @@
 import { Component, Input } from '@angular/core';
+import { Events } from 'ionic-angular';
+import { RatingData } from '../../providers/rating';
 
 @Component({
   selector: 'ppy-rating',
-  templateUrl: 'ppy-rating.html'
+  templateUrl: 'ppy-rating.html',
+  providers: [RatingData]
 })
 
 export class PpyRating {
 
   _rating: { page: string, entity_id: string, entity_type: string, score: number, meta: string };
+
+  ratingText = ['La información no me ayudo en nada.',
+              'La información es insufiente.',
+              'La infomación no es clara.',
+              'La infomación es suficiente.',
+              'La infomación es completa y clara.'];
+
+  constructor(public events: Events, public dataService: RatingData) {
+    events.subscribe('rating:retrieve', (rating, time) => {
+      this.setRating(rating, true);
+    });
+  }
 
   @Input()
   set rating(rating: { page: string, entity_id: string, entity_type: string, score: number, meta: string }) {
@@ -18,17 +33,24 @@ export class PpyRating {
     return this._rating;
   }
 
-  setRating(text, number){
-      this.rating.score = number;
-      document.getElementById("rating-text").innerHTML = text;
+  setRating(number, init){
+    this.rating.score = number;
+    document.getElementById("rating-text").innerHTML = this.ratingText[number-1];
+    if(!init){
       document.getElementById("send-rating").style.display = 'block';
-      for (let i = 1; i < 6; i++) {
-        var el = document.getElementById("star-"+i.toString());
-        if(i<=number){
-          el.classList.add("check");
-        }else{
-          el.classList.remove("check");
-        }
+    }
+    for (let i = 1; i < 6; i++) {
+      var el = document.getElementById("star-"+i.toString());
+      if(i<=number){
+        el.classList.add("check");
+      }else{
+        el.classList.remove("check");
       }
+    }
   }
+
+  pushRating(){
+    this.dataService.push(this.rating);
+  }
+
 }
