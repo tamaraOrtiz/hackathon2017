f8b05a9ff7f898df5607498b09ecc74b898c8dfe
Ruben Bordon <bordonwork@gmail.com>
2017-08-12 16:36:08 -0400

    mejore el grafico y agregue un select para elegir el tipo de presupuesto

--- hackathon2017/proyectandopy/src/components/ppy-canva/ppy-canva.ts
+++ hackathon2017/proyectandopy/src/components/ppy-canva/ppy-canva.ts
@@ -36,7 +36,6 @@ export class PpyCanva {
 
   @Input()
   set presupuestos(presupuestos: Array<{ nombre: string, nombre_programas: string, programas: Array<any>}>) {
-    console.log(Object.keys(presupuestos).map(key=>presupuestos[key]));
     this._presupuestos = Object.keys(presupuestos).map(key=>presupuestos[key]);
   }
 
@@ -45,144 +44,114 @@ export class PpyCanva {
   ngOnInit() {
 
     this.generatePie();
-    //this.getBarChart(this.presupuesto.nombre, this.presupuesto.nombre_programas, this.presupuesto.programas, this.getArrayDataDoughnut, this.backgroundColor, this.borderColor)
-  }
-
-  getChart(context, chartType, data, options?) {
-    return new Chart(context, {
-      type: chartType,
-      data: data,
-      options: options
-    });
-  }
-
-  getArrayDataBar(series, background, border){
-    var datasets= []
-    var i=0;
-    series.map(function(x) {
-      datasets.push({
-        label: x.name,
-        data: [x.value],
-        backgroundColor: background[i],
-        borderColor: border[i],
-        borderWidth: 1
-      });
-      i += 1;
-    });
-    return datasets;
-  }
-
-  getArrayDataDoughnut(series, background, border){
-
-    var data= [];
-    series.map(function(x) {
-      data.push(x.value);
-    });
-    var datasets = [{
-      backgroundColor: background,
-      borderColor: border,
-      data: data
-    }]
-    return datasets;
-  }
-
-  getBarChart(label, labels, series, getArrayData, background, border) {
-    let data = {
-      labels: labels.map(function(x) {
-        return x.toLowerCase();
-      }),
-      datasets: getArrayData(series, background, border),
-
-    };
-
-    let options = {
-      onAnimationComplete: function () {
-        console.log(this.graph.nativeElement.toDataURL("image/png"));
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }],
-        xAxes: [{
-          display: false
-        }],
-
-      },
-      legend: {
-        labels: {
-          // This more specific font property overrides the global property
-          fontSize: 10
-        }
-      }
-    }
-
-    return this.getChart(this.graph.nativeElement, "doughnut", data, options);
-
   }
 
   generatePie() {
     var dispatch = d3.dispatch("load", "statechange");
+
+    var presupuestosByName = d3.map();
+    this.presupuestos.forEach(function(d) { presupuestosByName.set(d.nombre, {nombre: d.nombre, programas: d.programas}); });
+
     // A drop-down menu for selecting a state; uses the "menu" namespace.
-    dispatch.on("load.menu", function(stateById) {
-      var select = d3.select("body")
-      .append("div")
-      .append("select")
-      .on("change", function() { dispatch.call("statechange", this, stateById.get(this.value)); });
-
+    dispatch.on("load.menu", function(presupuestosByName) {
+      var select = d3.select('.py-canva-select')
+      .on("change", function() { dispatch.call("statechange", this, presupuestosByName.get(this.value)); });
       select.selectAll("option")
-      .data(stateById.values())
+      .data(presupuestosByName.values())
       .enter().append("option")
-      .attr("value", function(d) { return d.id; })
-      .text(function(d) { return d.id; });
-
-      dispatch.on("statechange.menu", function(state) {
-        select.property("value", state.id);
+      .attr("value", function(d) { return d.nombre; })
+      .text(function(d) { return d.nombre; });
+      dispatch.on("statechange.menu", function(presupuesto) {
+        select.property("value", presupuesto.nombre);
       });
     });
-    let width  = 200;
-    let height = 200;
-    let radius = Math.min(width, height) / 2;
 
-    let data = this.presupuestos[0].programas.filter((item) => {
-      return [item.value, item.name];
+    dispatch.on("load.pie", function(presupuestosByName) {
+      let width  = this.graph.nativeElement.clientWidth;
+      let height = this.graph.nativeElement.clientWidth;
+      let radius = Math.min(width, height) / 2;
+      let data = presupuestosByName.get(presupuestosByName.keys()[0]).programas;
+      let color  = d3.scaleOrdinal()
+      .range(["#00838f",
+      "#388e3c", "#ad1457",
+      "#ff1744", "#1a237e",
+      "#004d40", "#607d8b"]);
+
+      let arc = d3.arc()
+      .outerRadius(radius * 0.5)
+      .innerRadius(radius - 20);
+
+      let pie = d3.pie()
+      .sort(null)
+      .value(function(d) { return d.value; });
+
+      let svg = d3.select(this.graph.nativeElement)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+      let g = svg.selectAll(".arc")
+      .data(pie(data))
+      .enter().append("g")
+      .attr("class", "arc");
+
+      g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data.value); });
+
+      g.append("text")
+      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .text(function(d) { return d.data.value; })
+      .style("fill", "white");
+
+      let tooltip = d3.select('#chart')                               // NEW
+          .append('div')                                                // NEW
+          .attr('class', 'tooltip');                                    // NEW
+
+        tooltip.append('div')                                           // NEW
+          .attr('class', 'label');                                      // NEW
+
+        tooltip.append('div')                                           // NEW
+          .attr('class', 'count');                                      // NEW
+
+        tooltip.append('div')                                           // NEW
+          .attr('class', 'percent');
+          g.on('mouseover', function(d) {                            // NEW
+                var total = d3.sum(data.map(function(d) {                // NEW
+                  console.log(d);
+                  return d.count;                                           // NEW
+                }));                                                        // NEW
+                var percent = Math.round(1000 * d.data.value / total) / 10; // NEW
+                tooltip.select('.label').html(d.data.name);                // NEW
+                tooltip.select('.count').html(d.data.value);                // NEW
+                tooltip.select('.percent').html(percent + '%');             // NEW
+                tooltip.style('display', 'block');                          // NEW
+              });
+      dispatch.on("statechange.pie", function(d) {
+        svg.selectAll("*").remove();
+        let g = svg.selectAll(".arc")
+        .data(pie(d.programas))
+        .enter().append("g")
+        .attr("class", "arc");
+
+        g.append("path")
+        .attr("d", arc)
+        .style("fill", function(d) { return color(d.data.value); });
+
+        g.append("text")
+        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+        .attr("dy", ".35em")
+        .text(function(d) { return d.data.value; })
+        .style("fill", "white");
+      });
     });
 
-    let color  = d3.scaleOrdinal()
-    .range(["#00838f",
-    "#388e3c", "#ad1457",
-    "#ff1744", "#1a237e",
-    "#004d40", "#607d8b"]);
+    dispatch.call("load", this, presupuestosByName);
+    dispatch.call("statechange", this, presupuestosByName.get(presupuestosByName.keys()[0]));
 
-    let arc = d3.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(radius - 70);
-
-    let pie = d3.pie()
-    .sort(null)
-    .value(function(d) { console.log(d); return d.value; });
-
-    let svg = d3.select(this.graph.nativeElement)
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-    let g = svg.selectAll(".arc")
-    .data(pie(data))
-    .enter().append("g")
-    .attr("class", "arc");
-
-    g.append("path")
-    .attr("d", arc)
-    .style("fill", function(d) { return color(d.data.value); });
-
-    g.append("text")
-    .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-    .attr("dy", ".35em")
-    .text(function(d) { return d.data.value; });
   }
 
 }
